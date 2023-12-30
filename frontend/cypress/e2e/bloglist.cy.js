40da3626a77f296d14abc3572d16cd66d5394c40
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-12-30 11:40:02 +0200

    Update bloglist.cy.js

--- frontend/cypress/e2e/bloglist.cy.js
+++ frontend/cypress/e2e/bloglist.cy.js
@@ -14,6 +14,7 @@ describe('bloglist', () => {
       cy.contains('LOGIN').click()
     })
 
+
     it('user can login', () => {
       cy.contains('LOGIN').click()
       cy.get('input:first').type('testusersix')
